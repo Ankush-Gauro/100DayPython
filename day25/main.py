4b250d9d89a65254717cba2ca2354e2881aa1db1
Ankush Gauro <agauro@mySeneca.ca>
2025-01-02 14:37:05 -0500

    day26 and us states game completed

--- day25/main.py
+++ day25/main.py
@@ -1,12 +1,41 @@
 import turtle
-
+import pandas as pd
 screen = turtle.Screen()
 screen.title("US states game")
-image = "blank_states_img.gif"
+image = "day25/blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
+guessed_states = []
+answer = screen.textinput(title="Guess the state", prompt="What's another state's name?").title()
+data = pd.read_csv("day25/50_states.csv")
+states = data.state.to_list()
+game_is_on = True
+while game_is_on:
+    if answer == 'Exit':
+        states_to_learn = []
+        for state in states:
+            if state not in guessed_states:
+                states_to_learn.append(state)
 
-answer = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+        df = pd.DataFrame(states_to_learn)
+        df.to_csv("day25/states_to_learn.csv")
+        break
+    if answer in states:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer)
+        states.remove(answer)
+    if len(guessed_states) == 50:
+        game_is_on = False
+    answer = screen.textinput(title=f"{len(guessed_states)}/50 states correct", prompt="What's another state's name?").title()
+
+
+
+df = pd.DataFrame(states_to_learn)
 
 turtle.mainloop()
---
+++ day26/new.py
@@ -0,0 +1,6 @@
+li = [1,2,3]
+new_li = [x+1 for x in li]
+print(new_li)
+
+new_li = [x*2 for x in range(1,5)]
+print(new_li)
