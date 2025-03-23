213354732aa49ca292bb19d75aaeb421abebde30
Ankush Gauro <agauro@mySeneca.ca>
2025-03-23 10:35:45 -0400

    wtFlask

--- day61/main.py
+++ day61/main.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from flask_bootstrap import Bootstrap5
+from wtforms.validators import DataRequired, Email, Length  # pip install email-validator
+from flask_bootstrap import Bootstrap5  # pip install bootstrap-flask
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
 
@@ -14,16 +28,10 @@ app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 bootstrap = Bootstrap5(app)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route("/login")
-def login():
-    login_form = LoginForm()
-    return render_template('login.html', form=login_form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -37,4 +45,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
